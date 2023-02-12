b478b3f046280ee9c111ef217ae5b018260a39d3
Loskh <1020612624@qq.com>
2023-02-12 23:34:27 +0800

    chore:remove duplicated log

--- Plugin.cs
+++ Plugin.cs
@@ -48,7 +48,6 @@ namespace ActionTimelineReplacement
                         var action = Actions?.GetRow((uint)config.Key);
                         var animationEnd = ActionTimelines?.GetRow((uint)config.Value.AnimationEnd);
                         var actionTimelineHit = ActionTimelines?.GetRow((uint)config.Value.ActionTimelineHit);
-                        PluginLog.Log($"Action:{action?.Name}({config.Key}):AnimationEnd->{animationEnd?.Key}({config.Value.AnimationEnd}),ActionTimelineHit->{actionTimelineHit?.Key}({config.Value.ActionTimelineHit})");
                         if (this.ActionTimelineReplacements.TryAdd(config.Key, config.Value)) {
                             PluginLog.Log($"Action:{action?.Name}({config.Key}):AnimationEnd->{animationEnd?.Key}({config.Value.AnimationEnd}),ActionTimelineHit->{actionTimelineHit?.Key}({config.Value.ActionTimelineHit})");
                         }
